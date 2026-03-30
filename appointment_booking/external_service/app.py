ccc6d4dfb63f87a1c0039bae2de1c292ad1df20b
Hunaid <brianpzaide@Hunaids-MacBook-Air.local>
2026-03-30 09:12:47 +0530

    add more endpoint to external service to improve the recipe flow

--- appointment_booking/external_service/app.py
+++ appointment_booking/external_service/app.py
@@ -5,6 +5,8 @@ from flask import Flask, request, jsonify
 
 default_duration = 15
 default_max_duration_minutes = 60
+morning_start = 10
+afternoon_start = 14
 
 
 def find_contigous_open_slots(n: int):
@@ -68,64 +70,59 @@ def find_available_slots(date_requested, duration_requested_minutes):
     if contigous_slots_required > 4:
         return []
     
-    def compute_available_slots():     
-        m_slots_i, a_slots_i = find_contigous_open_slots(contigous_slots_required)
+    m_slots_i, a_slots_i = find_contigous_open_slots(contigous_slots_required)
 
-        # convert the integers to the clock time
-        m_slots, a_slots = [], []
+    # convert the integers to the clock time
+    ats = []
     
-        for index in m_slots_i:
-            start_hrs = 10 + (index * default_duration)//60
-            start_mins = (index * default_duration)%60
-            end_hrs = start_hrs + (duration_requested_minutes)//60
-            end_mins = start_mins + (duration_requested_minutes)%60
-            carry = end_mins//60
-            end_hrs += carry
-            end_mins = end_mins%60
-            m_slots.append((start_hrs, start_mins, end_hrs, end_mins))
+    for index in m_slots_i:
+        start_hrs = morning_start + (index * default_duration)//60
+        start_mins = (index * default_duration)%60
+        end_hrs = start_hrs + (duration_requested_minutes)//60
+        end_mins = start_mins + (duration_requested_minutes)%60
+        carry = end_mins//60
+        end_hrs += carry
+        end_mins = end_mins%60
+        ats.append((start_hrs, start_mins, end_hrs, end_mins))
 
-        for index in a_slots_i:
-            start_hrs = 14 + (index * default_duration)//60
-            start_mins = (index * default_duration)%60
-            end_hrs = start_hrs + (duration_requested_minutes)//60
-            end_mins = start_mins + (duration_requested_minutes)%60
-            carry = end_mins//60
-            end_hrs += carry
-            end_mins = end_mins%60
-            a_slots.append((start_hrs, start_mins, end_hrs, end_mins))
+    for index in a_slots_i:
+        start_hrs = afternoon_start + (index * default_duration)//60
+        start_mins = (index * default_duration)%60
+        end_hrs = start_hrs + (duration_requested_minutes)//60
+        end_mins = start_mins + (duration_requested_minutes)%60
+        carry = end_mins//60
+        end_hrs += carry
+        end_mins = end_mins%60
+        ats.append((start_hrs, start_mins, end_hrs, end_mins))
 
-        return m_slots, a_slots
-    
-    week_around_requested_date = get_dates_around(date_requested)
-    available_time_slots = {}
-    for d in week_around_requested_date:
-        m_slots, a_slots = compute_available_slots()
-        available_time_slots[d] = {"morning_shift": m_slots, "afternoon_shift": a_slots}
-    
-    return available_time_slots
+    return ats
 
 
 app = Flask(__name__)
+
+@app.route("/dates", methods=["POST"])
+def get_dates():
+    req_data = request.get_json()
+    date_requested = req_data.get("date_requested")
+    dates_around_requested_date = get_dates_around(date_requested)
+
+    return jsonify({
+        "dates_around_requested_date": dates_around_requested_date
+    })
 
 @app.route("/availableslots", methods=["POST"])
 def get_available_slots():
     req_data = request.get_json()
     date_requested = req_data.get("date_requested")
-    available_timeslots = find_available_slots(req_data.get("date_requested"), req_data.get("duration_requested_minutes"))
+    available_timeslots = find_available_slots(date_requested, req_data.get("duration_requested_minutes"))
     ats = []
-    for d, v in available_timeslots.items():
-        m_slots = v['morning_shift']
-        for ts in m_slots:
-            start_minutes = "00" if ts[1] == 0 else ts[1]
-            end_minutes = "00" if ts[3] == 0 else ts[3]
-            ats.append(f"{d}\n{ts[0]}:{start_minutes}-{ts[2]}:{end_minutes}")
-        a_slots = v['afternoon_shift']
-        for ts in a_slots:
-            start_minutes = "00" if ts[1] == 0 else ts[1]
-            end_minutes = "00" if ts[3] == 0 else ts[3]
-            ats.append(f"{d}\n{ts[0]}:{start_minutes}-{ts[2]}:{end_minutes}")
-    
+    for ts in available_timeslots:
+        start_minutes = "00" if ts[1] == 0 else ts[1]
+        end_minutes = "00" if ts[3] == 0 else ts[3]
+        ats.append(f"{ts[0]}:{start_minutes}-{ts[2]}:{end_minutes}")
+        
     return jsonify({
+        "date": date_requested,
         "available_timeslots": ats
     })
 
